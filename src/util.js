a77ce230642577e3b9a6b6cdcbd52167297a852d
Aaron <aarongraham518@gmail.com>
2020-11-16 11:08:02 -0500

    initial commit

---
+++ src/util.js
@@ -0,0 +1,18 @@
+// export const playAudio = (isPlaying, audioRef) => {
+//     if(isPlaying){
+//         const playPromise = audioRef.current.play();
+//         if(playPromise !== undefined){
+//             playPromise.then((audio) =>{
+//                 audioRef.current.play();
+//             });
+//         }
+//     }
+// }
+
+// const Test = () => {
+
+// }
+// export default Test;
+
+// import {playAudio, orAnotherFunction} from './where playAudio is';
+// import jfdasfea from './util'
